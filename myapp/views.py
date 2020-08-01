bbe2022c8050acf5b11e18c393faafac1b32cae2
sw0610 <enthdnjs11@gmail.com>
2020-08-01 23:38:19 +0900

    loginprj edit

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def create(request):
@@ -7,4 +8,8 @@ def create(request):
     blog.body = request.GET['body']
     blog.pub_date = timezone.datetime.now()
     blog.save()
-    return
+    return    
+
+def home(request):
+    blogs = Blog.objects
+    return render(request,'home.html',{'blogs':blogs})
